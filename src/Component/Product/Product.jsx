a59d9ea64ba6b988c8c60150530c6c93bef18f13
developerabdullah-me <abdullahtalha9999@gmail.com>
2022-03-22 22:57:59 +0600

    cmmit2

--- src/Component/Product/Product.jsx
+++ src/Component/Product/Product.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import './Product.css'
 const Product = (props) => {
     console.log(props);
-    const {name, img}=props.product;
+    const {name, img,price,seller,ratings}=props.product;
     return (
         <div className="product-container">
             <img src={img}alt=""/>
-            <h1>{name}</h1>
+          <div className="info">
+          <p className="name">{name}</p>
+           <p className="price">Price:<small>${price}</small></p>
+           <p className="Manufacturer">Manufacturer:<small>{seller}</small></p>
+           <p className="ratings">Ratings:<small>{ratings}</small></p>
+          </div>
+          <button className="button-cart">Add to cart</button>
         </div>
     );
 };
