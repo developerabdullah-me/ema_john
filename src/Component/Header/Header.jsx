59ac56a2f49f55768136f4ec077978ef808ddd35
developerabdullah-me <abdullahtalha9999@gmail.com>
2022-04-13 18:00:01 +0600

    logo oute

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,9 +1,16 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../firebaseInit';
 import logo from '../../images/Logo.svg'
 import Shop from '../Shop/Shop';
 import './Header.css'
 const Header = () => {
+const [user]=useAuthState(auth)
+const logout = () => {
+  signOut(auth);
+};
     return (
         <div className="main-header">
           <nav className="header-nav">
@@ -13,7 +20,9 @@ const Header = () => {
             <Link to="/order">Order</Link>
             <Link to="/inventory">Inventory</Link>
             <Link to="/about">About</Link>
-            <Link to="/login">Login</Link>
+           {user? <button onClick={logout}>sign out</button>:
+              <Link to="/login">Login</Link>
+           }
       
         </div>
           </nav>
--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -1,54 +1,69 @@
-import React, { useState } from 'react';
-import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
-import { Link, useNavigate } from 'react-router-dom';
-import auth from '../../firebaseInit';
-import "./Login.css"
+import React, { useState } from "react";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import auth from "../../firebaseInit";
+import "./Login.css";
 const Login = () => {
-    const [password,setPassword]=useState('')
-    const [email,setEmail]=useState('')
-     
-    const [
-        signInWithEmailAndPassword,
-        user,
-        loading,
-        error,
-      ] = useSignInWithEmailAndPassword(auth);
-      const navigete=useNavigate()
-    const handelEMailBlur=event=>{
-        setEmail(event.target.value)
-    }
-    const handelPasswordBlur=event=>{
-        setPassword(event.target.value)
-    }
-    if(user){
-        navigete("/shop")
-    }
-    const fromSubmit=event=>{
-        event.preventDefault();
-        signInWithEmailAndPassword(email,password);
-    }
-    return (
-        <div  className="formContainer" >
-          <div>
-          <h2 className='form-title'>Login</h2>
-          <form onSubmit={fromSubmit}>
-           <div className="input-group">
-                <label htmlFor="email">Email</label>
-                <input onBlur={handelEMailBlur} type="email" placeholder="email" required/>
-            </div>
-            <div className="input-group">
-                <label htmlFor="password">password</label>
-                <input onBlur={handelPasswordBlur} type="password" placeholder="password" required/>
-            </div>
-            <p style={{color:'red'}}> {error?.message}</p>
-            <input className='form-submit' type="submit" value="Login" />
-           </form>
-           <p>
-                    New to Ema-John? <Link className='form-link' to="/signup">Create an account</Link>
-                </p>
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+
+  let navigete = useNavigate();
+
+  let location = useLocation();
+
+  let from = location.state?.from?.pathname || "/";
+
+  const handelEMailBlur = (event) => {
+    setEmail(event.target.value);
+  };
+  const handelPasswordBlur = (event) => {
+    setPassword(event.target.value);
+  };
+  if (user) {
+    navigete(from, { replace: true });
+  }
+  const fromSubmit = (event) => {
+    event.preventDefault();
+    signInWithEmailAndPassword(email, password);
+  };
+  return (
+    <div className="formContainer">
+      <div>
+        <h2 className="form-title">Login</h2>
+        <form onSubmit={fromSubmit}>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input
+              onBlur={handelEMailBlur}
+              type="email"
+              placeholder="email"
+              required
+            />
           </div>
-        </div>
-    );
+          <div className="input-group">
+            <label htmlFor="password">password</label>
+            <input
+              onBlur={handelPasswordBlur}
+              type="password"
+              placeholder="password"
+              required
+            />
+          </div>
+          <p style={{ color: "red" }}> {error?.message}</p>
+          <input className="form-submit" type="submit" value="Login" />
+        </form>
+        <p>
+          New to Ema-John?{" "}
+          <Link className="form-link" to="/signup">
+            Create an account
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
